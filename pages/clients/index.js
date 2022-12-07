4d5e4fd84ba6c32fc6bd90738dc757e3e3e02985
강한길 <59948163+gksrlf@users.noreply.github.com>
2022-12-07 14:46:02 +0900

    섹션3완료

---
+++ pages/clients/index.js
@@ -0,0 +1,25 @@
+import Link from "next/link"
+const ClientsPage = () => {
+	const clients = [
+		{ id: "max", name: "Maximilian" },
+		{ id: "manu", name: "Manuel" },
+	]
+	return (
+		<div>
+			<h1>The Clients Page</h1>
+			<ul>
+				<li>
+					{clients.map((client) => (
+						<li key={client.id}>
+							<Link href={`/clients/${client.id}`}>
+								{client.name}
+							</Link>
+						</li>
+					))}
+				</li>
+			</ul>
+		</div>
+	)
+}
+
+export default ClientsPage
